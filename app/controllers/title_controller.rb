3dbc8a2059ae3a2f71c8a8ee296f7468c025492a
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2024-06-26 20:24:14 +0200

    / is mapped to `title#index`

---
+++ app/controllers/title_controller.rb
@@ -0,0 +1,3 @@
+class TitleController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: redirect('question')
+  root 'title#index'
 
   get 'question', to: 'question#index'
 
