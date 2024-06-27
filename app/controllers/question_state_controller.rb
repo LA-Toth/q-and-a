77e19f58e44059d954bb257cf5e6e58a01656ec7
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2024-06-27 13:28:56 +0200

    question state: use the same view for every action

--- app/controllers/question_state_controller.rb
+++ app/controllers/question_state_controller.rb
@@ -1,21 +1,25 @@
 class QuestionStateController < ApplicationController
   def index
     @question_state = Rails.application.config.question_state
+    render :common
   end
 
   def first_topic
     @question_state = Rails.application.config.question_state
     @question_state.first_topic
+    render :common
   end
 
   def next_topic
     @question_state = Rails.application.config.question_state
     @question_state.next_topic
+    render :common
   end
 
   def next_topic_question
     @question_state = Rails.application.config.question_state
     @question_state.next_question
+    render :common
   end
 
   def next_question
@@ -28,5 +32,6 @@ class QuestionStateController < ApplicationController
       @question_state.topic = 1000
       @question_state.question = 1000
     end
+    render :common
   end
 end
