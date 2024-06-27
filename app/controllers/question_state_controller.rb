a86f65c1593f034fe3fbcded4dedfcba7c049dc0
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2024-06-27 15:07:18 +0200

    added answers; reorganize question_state UI

--- app/controllers/question_state_controller.rb
+++ app/controllers/question_state_controller.rb
@@ -1,37 +1,46 @@
 class QuestionStateController < ApplicationController
+  before_action :set_question_state
+
   def index
-    @question_state = Rails.application.config.question_state
+    set_vars
     render :common
   end
 
   def first_topic
-    @question_state = Rails.application.config.question_state
     @question_state.first_topic
-    render :common
+    redirect_to action: :index
   end
 
   def next_topic
-    @question_state = Rails.application.config.question_state
     @question_state.next_topic
-    render :common
+    redirect_to action: :index
   end
 
   def next_topic_question
-    @question_state = Rails.application.config.question_state
     @question_state.next_question
-    render :common
+    redirect_to action: :index
   end
 
   def next_question
-    @question_state = Rails.application.config.question_state
     if Question.exist?(@question_state.topic, @question_state.question + 1)
       @question_state.next_question
     elsif Question.exist?(@question_state.topic + 1, 1)
       @question_state.next_topic
     else
-      @question_state.topic = 1000
-      @question_state.question = 1000
+      @question_state.topic = Question::COMPLETED_VALUE
+      @question_state.question = Question::COMPLETED_VALUE
     end
-    render :common
+    redirect_to action: :index
+  end
+
+  private
+
+  def set_question_state
+    @question_state = Rails.application.config.question_state
+  end
+
+  def set_vars
+    @question = Question.new
+    @next_topic, @next_question = @question.next_topic_and_question
   end
 end
--- lib/question.rb
+++ lib/question.rb
@@ -1,4 +1,10 @@
 class Question < QuestionState
+  COMPLETED_VALUE = 1000
+  MISSING_QUESTION = '<missing question>'
+  MISSING_ANSWER = '<missing answer>'
+
+  BASE_DIR = ENV.fetch('TOPICS_QUESTIONS_DIR', Rails.root.join('topics-questions'))
+
   def initialize
     st = Rails.application.config.question_state
     super(st.topic, st.question)
@@ -8,12 +14,31 @@ class Question < QuestionState
     @text || load_question
   end
 
+  def answer
+    @answer || load_answer
+  end
+
   def load_question
-    filename = Rails.root.join(format('topics-questions/%02d-%02d.txt', topic, question))
-    @text = File.exist?(filename) ? File.read(filename) : '<missing question>'
+    filename = format('%s/%02d-%02d.txt', BASE_DIR, topic, question)
+    @text = File.exist?(filename) ? File.read(filename) : MISSING_QUESTION
+  end
+
+  def load_answer
+    filename = format('%s/%02d-%02d-answer.txt', BASE_DIR, topic, question)
+    @answer = File.exist?(filename) ? File.read(filename) : MISSING_ANSWER
   end
 
   def self.exist?(topic, question)
-    File.exist? Rails.root.join(format('topics-questions/%02d-%02d.txt', topic, question))
+    File.exist? format('%s/%02d-%02d.txt', BASE_DIR, topic, question)
+  end
+
+  def next_topic_and_question
+    if Question.exist?(topic, question + 1)
+      [topic, question + 1]
+    elsif Question.exist?(topic + 1, 1)
+      [topic + 1, 1]
+    else
+      [COMPLETED_VALUE, COMPLETED_VALUE]
+    end
   end
 end
